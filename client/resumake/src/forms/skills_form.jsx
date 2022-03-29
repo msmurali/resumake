1f5665430167e1b09c44c8277dfbb4d32d82e26a
msmurali <muralikrishnan0511@gmail.com>
2022-03-29 14:09:52 +0530

    create skills form

---
+++ client/resumake/src/forms/skills_form.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SkillsForm = ({ state, stateHandler, addField, deleteField }) => {
+  return <form className="form mx-auto p-6"></form>;
+};
+
+const FormGroup = ({ data, changeHandler, addField, deleteField, id }) => {
+  <div className="bg-gray-50 p-6 rounded-lg my-6"></div>;
+};
