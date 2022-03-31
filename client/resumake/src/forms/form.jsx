ae139d8e47aea9317c6dbd08777666cdf601b397
msmurali <muralikrishnan0511@gmail.com>
2022-03-31 12:50:57 +0530

    update input widget

--- client/resumake/src/forms/form.jsx
+++ client/resumake/src/forms/form.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { input } from "../constants/constants";
+import TextInput from "../widgets/text_input";
 
 const Form = ({ state, stateHandler, addGroup, removeGroup }) => {
   const submitHandler = (e) => e.preventDefault();
 
-  return <form className="form mx-auto p-6" onSubmit={submitHandler}></form>;
+  return (
+    <form className="form mx-auto p-6" onSubmit={submitHandler}>
+      {typeof state === Object
+        ? Object.keys(state).map((elem) => <Input />)
+        : ""}
+    </form>
+  );
 };
 
 export default Form;
--- client/resumake/src/widgets/input.jsx
+++ client/resumake/src/widgets/input.jsx
@@ -2,10 +2,46 @@ import React from "react";
 import TextInput from "./text_input";
 import TextArea from "./text_area";
 import RangeInput from "./range_input";
+import { input } from "../constants/constants";
 
-const Input = ({ inputType }) => {
-  switch (inputType) {
+const Input = ({ name, value, changeHandler }) => {
+  switch (input[name].inputtype) {
     case "text":
-      return <TextInput />;
+      return (
+        <TextInput
+          name={name}
+          value={value}
+          handleChange={changeHandler}
+          label={input[name].label}
+          placeholder={input[name].placeholder}
+          required={input[name].required}
+        />
+      );
+    case "text-area":
+      return (
+        <TextArea
+          name={name}
+          value={value}
+          handleChange={changeHandler}
+          label={input[name].label}
+          placeholder={input[name].placeholder}
+          required={input[name].required}
+        />
+      );
+    case "range":
+      return (
+        <RangeInput
+          name={name}
+          value={value}
+          handleChange={changeHandler}
+          label={input[name].label}
+          required={input[name].required}
+          max={5}
+          min={1}
+          step={1}
+        />
+      );
   }
 };
+
+export default Input;
